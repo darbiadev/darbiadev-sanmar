dfe0ec13a00004917df7a63eda809abe96f1662b
Bradley Reynolds <bradley.reynolds@darbia.dev>
2021-07-14 22:36:09 -0500

    Specify import from local package

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ copyright = '2021, Bradley Reynolds'
 author = 'Bradley Reynolds'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
